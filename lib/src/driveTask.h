41da523df2d5116df8e3216e304a60a519219574
mattwheatley98 <113391095+mattwheatley98@users.noreply.github.com>
2025-01-21 10:19:33 -0500

    Update driveTask.h

--- lib/src/driveTask.h
+++ lib/src/driveTask.h
@@ -2,11 +2,7 @@
 // Created by matt on 4/17/23.
 //
 
-#ifndef CARPROJECT_DRIVETASK_H
-#define CARPROJECT_DRIVETASK_H
-
-#endif //CARPROJECT_DRIVETASK_H
-
+#pragma once
 
 void driveTask(void *parameter);
 extern SemaphoreHandle_t frontBackObjectDetectionHandle;
